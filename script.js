f036206f4bcbc34b5a6b5c1abbbf41ef01b9f825
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2023-05-07 12:20:33 +0530

    add: auto open collapsible on reaching center of the viewport

--- script.js
+++ script.js
@@ -14,11 +14,34 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-// keep the first project expanded by default
-// var collapsibles = document.querySelectorAll(".collapsible");
-// collapsibles.forEach(function (collapsible) {
-//   collapsible.click();
-// });
-document.querySelector(".collapsible").click();
-document.querySelector("#testimonials .collapsible").click();
 
+// Checking if the collapsible reached to the center of the screen
+function isInCenter(element) {
+    const rect = element.getBoundingClientRect();
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+    // Check if the element is in the center of the page
+    return (
+        rect.top <= windowHeight / 2 &&
+        rect.bottom >= windowHeight / 2 &&
+        rect.left <= windowWidth / 2 &&
+        rect.right >= windowWidth / 2
+    );
+}
+
+window.addEventListener('scroll', function() {
+    // Get all elements with the specified class name
+    const elements = document.querySelectorAll('.collapsible');
+
+    // Loop over all elements
+    for (const element of elements) {
+        // Check if the element has the 'active' class and if the element is in the viewport
+        if (!element.classList.contains('active') && isInCenter(element)) {
+            // The element is visible, do something
+            console.log('An element is in center!, ', element);
+            // The element doesn't have the 'active' class, add it
+            element.click()
+        }
+    }
+});
